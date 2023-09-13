98c81956b929b07cc366540172873324254f9218
Abhash Rai <avash.cmling@gmail.com>
2023-09-13 09:39:57 +0545

    portfolio section structure formation

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,13 +1,70 @@
+import attendance_management_system from "../../images/project_images/attendance_management_system.png";
+import customer_churn from "../../images/project_images/customer_churn.png";
+import sydney_housing_analysis from "../../images/project_images/sydney_housing_analysis.png";
+import co2_prediction from "../../images/project_images/co2_prediction.png";
+import "./Portfolio.css";
+
 const Portfolio = ({
   container_classes,
   headers_classes,
   normal_text_classes,
 }) => {
+  const projects = [
+    {
+      coverImg: attendance_management_system,
+      projectTitle: "Attendance Management System",
+      projectType: "COMPUTER VISION & DATA ANALYSIS",
+    },
+    {
+      coverImg: customer_churn,
+      projectTitle: "Customer Churn Prediction",
+      projectType: "MACHINE LEARNING CLASSIFICATION & DATA ANALYSIS",
+    },
+    {
+      coverImg: sydney_housing_analysis,
+      projectTitle: "Sydney Housing Market Analysis",
+      projectType: "DATA ANALYSIS",
+    },
+    {
+      coverImg: co2_prediction,
+      projectTitle: "CO2 Emissions Prediction",
+      projectType: "MACHINE LEARNING REGRESSION & DATA ANALYSIS",
+    },
+  ];
+
+  const ProjectCard = ({ project }) => {
+    return (
+      <div
+        className="project-card 
+        w-[80%] sm:w-[70%] md:w-[70%] lg:w-[45%] xl:w-[45%] box-border 
+        h-[220px] sm:h-[250px] md:h-[280px] lg:h-[300px] xl:h-[350px]"
+        style={{
+          backgroundImage: `url(${project.coverImg})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        {/* <img src={project.coverImg} alt={project.projectTitle} /> */}
+        <h2>{project.projectTitle}</h2>
+        <p>{project.projectType}</p>
+      </div>
+    );
+  };
+
   return (
     <div
-      className={`portfolio_section flex border-2 border-red ${container_classes} pt-28 sm:pt-48 md:pt-48 lg:pt-40 xl:pt-40`}
+      className={`portfolio_section flex flex-col gap-10 ${container_classes}`}
     >
-      Portfolio
+      <div className="w-full flex flex-col">
+        <div>PORTFOLIO</div>
+        <div>Some of my works</div>
+      </div>
+      <div class="flex flex-wrap gap-10 justify-center">
+        {projects.map((project, index) => (
+          <ProjectCard key={index} project={project} />
+        ))}
+      </div>
     </div>
   );
 };
