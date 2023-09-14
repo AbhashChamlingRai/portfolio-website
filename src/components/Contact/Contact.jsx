30a9e1cfaabdc705178cc24c98517b00879af781
Abhash Rai <avash.cmling@gmail.com>
2023-09-14 22:25:13 +0545

    adjusted width of send button, removed unused code

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -131,7 +131,7 @@ function ContactForm({
           <div>
             <button
               type="submit"
-              className={`focus:border-teal focus:outline-none focus:ring-0 ${normal_text_classes} font-semibold bg-custom-light-blue text-custom-darker-blue border-0 w-[15%] h-[40px] rounded-lg hover:bg-[#39b1b7]`}
+              className={`focus:border-teal focus:outline-none focus:ring-0 ${normal_text_classes} font-semibold bg-custom-light-blue text-custom-darker-blue border-0 w-[30%] sm:w-[15%] h-[40px] rounded-lg hover:bg-[#39b1b7]`}
             >
               Send
             </button>
@@ -139,57 +139,6 @@ function ContactForm({
         </form>
       </div>
     </div>
-  );
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="fullName">Full Name</label>
-        <input
-          type="text"
-          id="fullName"
-          name="fullName"
-          value={formValues.fullName}
-          onChange={setFormValues}
-          placeholder="Your Full Name"
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Your Email</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formValues.email}
-          onChange={setFormValues}
-          placeholder="Your Email"
-        />
-      </div>
-      <div>
-        <label htmlFor="subject">Message Subject</label>
-        <input
-          type="text"
-          id="subject"
-          name="subject"
-          value={formValues.subject}
-          onChange={setFormValues}
-          placeholder="Message Subject"
-        />
-      </div>
-      <div>
-        <label htmlFor="message">Your Message Goes Here...</label>
-        <textarea
-          id="message"
-          name="message"
-          value={formValues.message}
-          onChange={setFormValues}
-          placeholder="Your Message Goes Here..."
-        />
-      </div>
-      <div>
-        <button type="submit">Send</button>
-      </div>
-    </form>
   );
 }
 
